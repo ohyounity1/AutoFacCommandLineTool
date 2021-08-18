0dd1b659223c2965bcd07e01c9512370a10934b7
Chris Wodarczyk <u031583@Global.Bio-Rad.com>
2021-08-17 18:22:11 -0700

    Autofac Modules Added

--- Integration/ConsoleApp/Program.cs
+++ Integration/ConsoleApp/Program.cs
@@ -7,6 +7,8 @@ using ConsoleApp.Framework.Commands;
 using ConsoleApp.Framework.Input;
 using ConsoleApp.Framework.User;
 using ConsoleApp.Framework.CommandLine;
+using Microsoft.Extensions.Configuration;
+using Autofac.Configuration;
 
 namespace ConsoleApp
 {
@@ -20,9 +22,23 @@ namespace ConsoleApp
         
         static void Main(string[] args)
         {
+			/*
             var program = new Program();
-            program.Execute(args);
-        }
+            program.Execute(args);*/
+
+			// Add the configuration to the ConfigurationBuilder.
+			var config = new ConfigurationBuilder();
+			// config.AddJsonFile comes from Microsoft.Extensions.Configuration.Json
+			// config.AddXmlFile comes from Microsoft.Extensions.Configuration.Xml
+			config.AddJsonFile(@"Resources\config.json");
+
+			// Register the ConfigurationModule with Autofac.
+			var module = new ConfigurationModule(config.Build());
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(module);
+
+			var container = builder.Build();
+		}
 
         protected void Execute(string[] args)
         {
